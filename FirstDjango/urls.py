51adcf63eba9acd23e03aa3ad2deefd155011480
Gulin Ivan <19980721qwe@gmail.com>
2024-10-08 20:52:49 +0300

    After add changes

--- FirstDjango/urls.py
+++ FirstDjango/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.urls import path
 from MainApp import views
+
 urlpatterns  = [
     path ("",views.home),
+    path ("/about",views.about)
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -10,3 +10,14 @@ def home(request):
     <strong>Author<strong>:<i>Ivanov</i>
     """
     return HttpResponse(text)
+
+def about(request):
+    text  =  """
+    <h2>Имя: Иван</h2>
+    <h2>Отчество: Петрович</h2>
+    <h2>Фамилия: Иванов</h2>
+    <h2>телефон: 8-923-600-01-02</h2>
+    <h2>email: vasya@mail.ru</h2>
+    <strong>Author<strong>:<i>Ivanov</i>
+    """
+    return HttpResponse(text)
