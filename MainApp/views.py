c5abb27787c4143babbd6339253ee3fc75147e27
Gulin Ivan <19980721qwe@gmail.com>
2024-10-08 20:01:33 +0300

    After add changes

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    text  =  """
+    <h1>we  are learnin django</h1>
+    <strong>Author<strong>:<i>Ivanov</i>
+    """
+    return HttpResponse(text)
