0c5894be4ab357e0ba44f90394c0f49ba7542ed9
Gulin Ivan <19980721qwe@gmail.com>
2024-10-10 17:53:42 +0300

    Done.m2:task2-3 and add comment examples

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,6 +3,23 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 # Create your views here.
+
+
+def home(request):
+    context = {
+
+        "name": "Иванов  Петр  Семенович",
+        "email": "my_mail@mail.ru"
+    }
+    return render(request, "index.html" , context)
+
+
+def items_page(request):
+    context = {
+
+
+    }
+
 author = {
     "Имя": "Иван",
     "Отчество": "Петрович",
@@ -19,16 +36,6 @@ items = [
     {"id": 8, "name": "Кепка" ,"quantity":124},
 ]
 
-
-
-def home(request):
-    text  =  """
-    <h1>we  are learnin django</h1>
-    <strong>Author<strong>:<i>Ivanov</i>
-    """
-    return HttpResponse(text)
-
-
 def about(request):
     text=f"""
         Имя: {author["Имя"]}<br>
@@ -39,21 +46,31 @@ def about(request):
     """
     return HttpResponse(text)  # или return "<br>".join([f"{key}: {value}" for key, value in user_info.items()])
 
-def get_item(request , item_id:int):
-    """Trugger item_id back element list """
+def get_item(request , item_id: int):
+    """Trigger item_id back element list """
     for item  in items:
         if item  ['id'] == item_id:
             result = f"""
-            <h2>  Name: {item['name']} </h2>
-            <p> Cost:{item['quiantity']}</p>
+            <h2> Name: {item['name']}</h2>
+            <p> Cost:{item['quantity']}</p>
             <p> <a href ='/items'> back to items list</a>
             """
-            return HttpResponse(f'{result = }')
+            return HttpResponse(result)
     return HttpResponseNotFound(f'Item with id={item_id} not found')
 
+# def get_items(request):
+#     result =  "<h1> List product</h1><ol>"
+#     for item in items:
+#         result += f"""<li><a href='/item/{item["id"]}'> {item["name"]} </a> </li>"""
+#     result += "</ol>"
+#     return HttpResponse(result)
+
 def get_items(request):
-    result =  "<h1> List product</h1><ol>"
-    for item in items:
-        result += f"""<li><a href='/item/{item["id"]}'> {item["name"]} </a> </li>"""
-    result += "</ol>"
-    return HttpResponse(result)
+    context = {
+
+        "items":items
+    }
+    return render(request,"items_list.html", context)
+    
+
+
