a39f72f210bf2be046fca1b38d864f3973b5d85a
Gulin Ivan <19980721qwe@gmail.com>
2024-10-10 19:37:52 +0300

    Add item model and make migration

--- MainApp/views.py
+++ MainApp/views.py
@@ -5,28 +5,6 @@ from django.http import HttpResponse, HttpResponseNotFound
 # Create your views here.
 
 
-def home(request):
-    context = {
-
-        "name": "Иванов  Петр  Семенович",
-        "email": "my_mail@mail.ru"
-    }
-    return render(request, "index.html" , context)
-
-
-def items_page(request):
-    context = {
-
-
-    }
-
-author = {
-    "Имя": "Иван",
-    "Отчество": "Петрович",
-    "Фамилия": "Иванов",
-    "телефон": "8-923-600-01-02",
-    "email": "vasya@mail.ru",
-}
 
 items = [
     {"id": 1, "name": "Кроссовки abibas" ,"quantity":5},
@@ -36,26 +14,35 @@ items = [
     {"id": 8, "name": "Кепка" ,"quantity":124},
 ]
 
+def home(request):
+    context = {
+        "name": "Иванов Петр Семенович",
+        "email": "my_mail@mail.ru"
+    }
+    return render(request, "index.html", context)
+
 def about(request):
-    text=f"""
-        Имя: {author["Имя"]}<br>
-        Отчество:{author["Отчество"]}<br>
-        Фамилия: {author["Фамилия"]}<br>
-        телефон: {author["телефон"]}<br>
-        email: {author["email"]}<br>
-    """
-    return HttpResponse(text)  # или return "<br>".join([f"{key}: {value}" for key, value in user_info.items()])
+    author = {
+    "name": "Иван",
+    "middle_name": "Петрович",
+    "last_name": "Иванов",
+    "phone": "8-923-600-01-02",
+    "email": "vasya@mail.ru",
+    }   
+    return render(request, "about.html", {"author": author})
+
+
+
+
+
 
 def get_item(request , item_id: int):
     """Trigger item_id back element list """
     for item  in items:
         if item  ['id'] == item_id:
-            result = f"""
-            <h2> Name: {item['name']}</h2>
-            <p> Cost:{item['quantity']}</p>
-            <p> <a href ='/items'> back to items list</a>
-            """
-            return HttpResponse(result)
+            context={"item":item}
+            
+            return 
     return HttpResponseNotFound(f'Item with id={item_id} not found')
 
 # def get_items(request):
